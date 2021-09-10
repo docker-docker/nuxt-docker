c0576b1ef2ccd5cad51e482980e0b9ccd00f2eef
Walter Hu <alterhu2020@gmail.com>
2021-09-10 16:21:02 +0800

    fix: pm2 issues

--- nuxt-typescript-docker/ecosystem.config.js
+++ nuxt-typescript-docker/ecosystem.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   apps: [{
     name: 'www',
-    cwd: './',
-    script: './node_modules/nuxt-start/bin/nuxt-start.js',
     instances: 'max',
     exec_mode: 'cluster',
     autorestart: true,
+    max_restarts: 3,
+    restart_delay: 5000,
+    error_file: '/www/logs/pm2err.log',
+    out_file: '/www/logs/pm2out.log',
+    script: './node_modules/nuxt/bin/nuxt.js',
+    args: 'start',
+    // enable watch & restart feature, if a file change in the folder or subfolder, your app will get reloaded
     watch: [
-      '_nuxt',
-      'dist'
+      'src'
     ]
   }]
 }
